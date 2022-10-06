89e97ef625ba6b2fc68c68788ebb477a18a5abf4
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-06 14:18:57 -0400

    added a delete route for categories, fixed put route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,15 +38,21 @@ Category.create(req.body)
 router.put('/:id', (req, res) => {
   //Class info - update a category by its `id` value
 // Updates one category by its Id value
-Category.update (req.body, {where:{id:req.params.id}})
+Category.update (req.body, {where: {id: req.params.id}})
   .then(dataInfo => {
     console.log(dataInfo)
     res.status(200).json(dataInfo)
-  }).catchThrow(err => res.status(500).json(err))
+  }).catch(err => res.status(500).json(err))
 });
 
 router.delete('/:id', (req, res) => {
   //Class info - delete a category by its `id` value
+// Delete a Category route by its Id Value
+Category.destroy({where: {id: req.params.id}})
+  .then(dataInfo => {
+    console.log(dataInfo)
+    res.status(200).json(dataInfo)
+  }).catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
