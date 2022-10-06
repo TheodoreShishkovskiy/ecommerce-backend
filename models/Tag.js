945c08b7471b49f8ce233ec8ed27492610279073
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-06 13:17:43 -0400

    added id columns for ProductTags model

--- models/Tag.js
+++ models/Tag.js
@@ -9,7 +9,8 @@ Tag.init(
 // Defines the id column for Tag
     id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
 // Defines the tag_name column for Tag
-    tag_name: { type: DataTypes.STRING}
+    tag_name: 
+    { type: DataTypes.STRING}
   },
   {
     sequelize,
