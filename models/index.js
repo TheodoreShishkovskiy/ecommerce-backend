bf1bf781c0349435b597bab0cf4c9963bec2d0ab
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-06 12:49:30 -0400

    added index models, specifyng what goes where

--- models/index.js
+++ models/index.js
@@ -1,16 +1,22 @@
-// import models
+// Class info -  import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Class info -  Products belongsTo Category
+// This allows all products to belong to the Category model
+Product.belongsTo(Category);
+// Class info -  Categories have many Products
+// Shows that Category model contains many products
+Category.hasMany(Product);
+// Class info -  Products belongToMany Tags (through ProductTag)
+// Product belongs to many Tags
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id'});
+// Class info -  Tags belongToMany Products (through ProductTag)
+// Tag belongs to many Products
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id'});
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
