a28adb42c6789d60e69199b21241b3420b15a2db
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-06 19:59:36 -0400

    added a findall route for tag model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { findAll } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+// Find all tags by id including the product data
+Tag.findAll ({
+  attributes: ['id', 'tag_name'],
+  include: [{model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}]
+})
+.then(dataInfo => {
+  console.log(dataInfo)
+  res.status(200).json(dataInfo)
+}).catch(err => res.status(400).json(err))
 });
 
 router.get('/:id', (req, res) => {
