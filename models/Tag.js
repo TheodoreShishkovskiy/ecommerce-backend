bfc0ff15fdef310fc3388acf761920ccf13e61a8
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-06 13:09:03 -0400

    added tag columns to model tag

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Tag extends Model {}
+
+// Tag column Model
 
 Tag.init(
   {
-    // define columns
+// Defines the id column for Tag
+    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
+// Defines the tag_name column for Tag
+    tag_name: { type: DataTypes.STRING}
   },
   {
     sequelize,
