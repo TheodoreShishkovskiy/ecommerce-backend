3cc47b4ddf4b953ea679f0855f584348de257ef9
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-06 20:09:19 -0400

    added findOne route for Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 const { findAll } = require('../../models/Product');
 
-// The `/api/tags` endpoint
+//Class info - The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  //Class info - find all tags
+  //Class info - be sure to include its associated Product data
 // Find all tags by id including the product data
 Tag.findAll ({
   attributes: ['id', 'tag_name'],
@@ -19,20 +19,29 @@ Tag.findAll ({
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  //Class info - find a single tag by its `id`
+  //Class info - be sure to include its associated Product data
+// Find one Tag by its id with its assoication to Product Data
+Tag.findOne ({where: {id: req.params.id},
+  attributes: ['id', 'tage_name'],
+  include: [{model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}]
+})
+.then(dataInfo => {
+  console.log(dataInfo)
+  res.status(200).json(dataInfo)
+}).catch(err => res.status(400).json(err))
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  //Class info - create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  //Class info - update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  //Class info - delete on tag by its `id` value
 });
 
 module.exports = router;
